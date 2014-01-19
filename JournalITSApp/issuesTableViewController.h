646c7bb21235743cfba789fd99f3e78eb76b203c
Barry <bachma0507@gmail.com>
2014-01-19 03:21:50 -0500

    Added tableviewcontroller for issues

---
+++ JournalITSApp/issuesTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  issuesTableViewController.h
+//  JournalITSApp
+//
+//  Created by Barry on 1/19/14.
+//  Copyright (c) 2014 BICSI. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface issuesTableViewController : UITableViewController
+
+@end
